d3eae593862829a9cd5e5aeed3841e163d651d84
许冬冬 <awsd313025@163.com>
2025-06-19 11:24:06 +0800

    功能：增强Vditor编辑器功能，添加Markdown快捷提示、暗黑主题支持及完整工具栏配置

--- src/pages/Vditor/index.tsx
+++ src/pages/Vditor/index.tsx
@@ -17,13 +17,74 @@ function VditorNote(props: IVditorNoteProps) {
         cache: {
           id: "vditorNote",
         },
+        hint: {
+          extend: [
+            {
+              key: "/",
+              hint: (value) => {
+                return [
+                  {
+                    html: `<p>一级标题</p>`,
+                    value: `# ${value}`,
+                  },
+                  {
+                    html: `<p>二级标题</p>`,
+                    value: `## ${value}`,
+                  },
+                ];
+              },
+            },
+          ],
+        },
         lang: "zh_CN",
         tab: "\t",
-        // toolbar: [],
+        theme: "dark",
+        preview: {
+          markdown: {
+            paragraphBeginningSpace: true,
+          },
+        },
+        toolbar: [
+          "emoji",
+          "headings",
+          "bold",
+          "italic",
+          "strike",
+          "|",
+          "line",
+          "quote",
+          "list",
+          "ordered-list",
+          "check",
+          "outdent",
+          "indent",
+          "code",
+          "inline-code",
+          "insert-after",
+          "insert-before",
+          "undo",
+          "redo",
+          "upload",
+          "link",
+          "table",
+          "record",
+          "edit-mode",
+          "both",
+          "preview",
+          "fullscreen",
+          "outline",
+          "code-theme",
+          "content-theme",
+          "export",
+          "devtools",
+          "info",
+          "help",
+          "br",
+        ],
       });
     }
   }, []);
 
-  return <div ref={vditorWrapperRef}></div>;
+  return <div ref={vditorWrapperRef} className="!text-accent-foreground"></div>;
 }
 export default VditorNote;
