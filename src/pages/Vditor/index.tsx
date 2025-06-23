39832bed874d0cb5385c64e48444c54dadae5c94
许冬冬 <awsd313025@163.com>
2025-06-23 18:40:34 +0800

    重构：增强Vditor编辑器功能，新增三级标题支持与主题切换选项，集成Card和Menubar组件优化UI布局，添加自定义样式文件

--- src/pages/Vditor/index.tsx
+++ src/pages/Vditor/index.tsx
@@ -1,7 +1,29 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardHeader,
+} from "@/components/ui/card";
+import {
+  Menubar,
+  MenubarCheckboxItem,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarRadioGroup,
+  MenubarRadioItem,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarSub,
+  MenubarSubContent,
+  MenubarSubTrigger,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 import { useEffect, useRef } from "react";
-import "vditor/src/assets/less/index.less";
-
+import { toast } from "sonner";
 import Vditor from "vditor";
+import "vditor/src/assets/less/index.less";
+import "./index.less";
 
 export interface IVditorNoteProps {}
 
@@ -17,6 +39,7 @@ function VditorNote(props: IVditorNoteProps) {
         cache: {
           id: "vditorNote",
         },
+
         hint: {
           extend: [
             {
@@ -31,6 +54,10 @@ function VditorNote(props: IVditorNoteProps) {
                     html: `<p>二级标题</p>`,
                     value: `## ${value}`,
                   },
+                  {
+                    html: `<p>三级标题</p>`,
+                    value: `### ${value}`,
+                  },
                 ];
               },
             },
@@ -42,6 +69,15 @@ function VditorNote(props: IVditorNoteProps) {
         preview: {
           markdown: {
             paragraphBeginningSpace: true,
+          },
+          theme: {
+            current: "dark",
+            list: {
+              "ant-design": "Ant Design",
+              dark: "Dark",
+              light: "Light",
+              wechat: "WeChat",
+            },
           },
         },
         toolbar: [
@@ -85,6 +121,111 @@ function VditorNote(props: IVditorNoteProps) {
     }
   }, []);
 
-  return <div ref={vditorWrapperRef} className="!text-accent-foreground"></div>;
+  return (
+    <Card className="h-full">
+      <CardHeader>
+        <CardAction>
+          <Menubar>
+            <MenubarMenu>
+              <MenubarTrigger>文件</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  New Window <MenubarShortcut>⌘N</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem disabled>New Incognito Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarSub>
+                  <MenubarSubTrigger>Share</MenubarSubTrigger>
+                  <MenubarSubContent>
+                    <MenubarItem
+                      onClick={async () => {
+                        const value = vditorRef.current?.getValue();
+                        await navigator.clipboard.writeText(value ?? "");
+                        toast.success("复制成功");
+                      }}
+                    >
+                      复制全文到剪贴板
+                    </MenubarItem>
+                    <MenubarItem>Messages</MenubarItem>
+                    <MenubarItem>Notes</MenubarItem>
+                  </MenubarSubContent>
+                </MenubarSub>
+                <MenubarSeparator />
+                <MenubarItem>
+                  Print... <MenubarShortcut>⌘P</MenubarShortcut>
+                </MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Edit</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  Undo <MenubarShortcut>⌘Z</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>
+                  Redo <MenubarShortcut>⇧⌘Z</MenubarShortcut>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarSub>
+                  <MenubarSubTrigger>Find</MenubarSubTrigger>
+                  <MenubarSubContent>
+                    <MenubarItem>Search the web</MenubarItem>
+                    <MenubarSeparator />
+                    <MenubarItem>Find...</MenubarItem>
+                    <MenubarItem>Find Next</MenubarItem>
+                    <MenubarItem>Find Previous</MenubarItem>
+                  </MenubarSubContent>
+                </MenubarSub>
+                <MenubarSeparator />
+                <MenubarItem>Cut</MenubarItem>
+                <MenubarItem>Copy</MenubarItem>
+                <MenubarItem>Paste</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>视图</MenubarTrigger>
+              <MenubarContent>
+                <MenubarCheckboxItem>全屏</MenubarCheckboxItem>
+                <MenubarCheckboxItem checked>
+                  Always Show Full URLs
+                </MenubarCheckboxItem>
+                <MenubarSeparator />
+                <MenubarItem inset>
+                  Reload <MenubarShortcut>⌘R</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem disabled inset>
+                  Force Reload <MenubarShortcut>⇧⌘R</MenubarShortcut>
+                </MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Toggle Fullscreen</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Hide Sidebar</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+            <MenubarMenu>
+              <MenubarTrigger>Profiles</MenubarTrigger>
+              <MenubarContent>
+                <MenubarRadioGroup value="benoit">
+                  <MenubarRadioItem value="andy">Andy</MenubarRadioItem>
+                  <MenubarRadioItem value="benoit">Benoit</MenubarRadioItem>
+                  <MenubarRadioItem value="Luis">Luis</MenubarRadioItem>
+                </MenubarRadioGroup>
+                <MenubarSeparator />
+                <MenubarItem inset>Edit...</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem inset>Add Profile...</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <div ref={vditorWrapperRef}></div>
+      </CardContent>
+    </Card>
+  );
 }
 export default VditorNote;
